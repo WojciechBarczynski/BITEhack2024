3fdbdcc47af95ed4439d6b386f81b1f5981b672e
Wojciech Barczyński <104033489+WojciechBarczynski@users.noreply.github.com>
2024-01-20 16:50:48 +0100

    Addiction service (#12)

--- backend/src/main/java/com/example/backend/addiction/AddictionController.java
+++ backend/src/main/java/com/example/backend/addiction/AddictionController.java
@@ -13,27 +13,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/addiction")
 public class AddictionController {
-
-    private final UserRepository userRepository;
-    private final AddictionRepository addictionRepository;
+    private final AddictionService addictionService;
 
     @Autowired
-    public AddictionController(UserRepository userRepository, AddictionRepository addictionRepository) {
-        this.userRepository = userRepository;
-        this.addictionRepository = addictionRepository;
+    public AddictionController(AddictionService addictionService) {
+        this.addictionService =addictionService;
     }
 
     @PostMapping("/{addictionName}")
-    public ResponseEntity<?> addAddiction(@PathVariable("addictionName") String addictionName, @RequestParam Integer userId) {
-        Optional<User> userOptional = userRepository.findById(userId);
-        if(userOptional.isPresent()){
-            User user = userOptional.get();
-            Addiction addiction = new Addiction(addictionName);
-            addiction.addAddict(user);
-            addictionRepository.save(addiction);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<?> createAddiction(@PathVariable("addictionName") String addictionName) {
+        var addiction = addictionService.createAddiction(addictionName);
+        return ResponseEntity.ok(addiction);
     }
 }
