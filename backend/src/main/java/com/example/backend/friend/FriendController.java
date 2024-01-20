ef7072916fc92fa60bb40d7ac1dae77bdd8003ab
Wojciech Barczynski <barczynski.w@gmail.com>
2024-01-20 17:08:37 +0100

    Friend service

--- backend/src/main/java/com/example/backend/friend/FriendController.java
+++ backend/src/main/java/com/example/backend/friend/FriendController.java
@@ -1,9 +1,5 @@
 package com.example.backend.friend;
 
-import com.example.backend.addiction.Addiction;
-import com.example.backend.addiction.AddictionRepository;
-import com.example.backend.user.User;
-import com.example.backend.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,40 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/friend")
 public class FriendController {
 
-    private final UserRepository userRepository;
-    private final FriendRelationRepository friendRelationRepository;
-    private final AddictionRepository addictionRepository;
+    private final FriendService friendService;
 
     @Autowired
-    public FriendController(UserRepository userRepository, FriendRelationRepository friendRelationRepository, AddictionRepository addictionRepository) {
-        this.userRepository = userRepository;
-        this.friendRelationRepository = friendRelationRepository;
-        this.addictionRepository = addictionRepository;
+    public FriendController(FriendService friendService) {
+        this.friendService = friendService;
     }
 
     @PostMapping()
     public ResponseEntity<?> addFriendRelation(@RequestParam Integer addictId,
                                                @RequestParam Integer friendId,
                                                @RequestParam Integer addictionId) {
-        Optional<User> addictOptional = userRepository.findById(addictId);
-        Optional<User> friendOptional = userRepository.findById(friendId);
-        Optional<Addiction> addictionOptional = addictionRepository.findById(addictionId);
-
-        if (addictOptional.isPresent() && friendOptional.isPresent() && addictionOptional.isPresent()) {
-            FriendRelation friendRelation = new FriendRelation();
-            friendRelation.setAddict(addictOptional.get());
-            friendRelation.setFriend(friendOptional.get());
-            friendRelation.setAddiction(addictionOptional.get());
-            friendRelationRepository.save(friendRelation);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        friendService.createFriendRelation(addictId, friendId, addictionId);
+        return ResponseEntity.ok().build();
     }
 }
