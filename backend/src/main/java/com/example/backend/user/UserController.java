f0f3b2e3b862d9cfd17680be4dcbce070137b800
Wojciech Barczyński <104033489+WojciechBarczynski@users.noreply.github.com>
2024-01-20 17:18:20 +0100

    Get all users (#14)

--- backend/src/main/java/com/example/backend/user/UserController.java
+++ backend/src/main/java/com/example/backend/user/UserController.java
@@ -6,10 +6,12 @@ import com.example.backend.user.requests.RegisterRequest;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -31,6 +33,12 @@ public class UserController {
     public ResponseEntity<UserDto> login(@RequestBody LoginRequest request) {
         var user = userService.loginUser(request.nick(), request.password());
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        var users = userService.getAll();
+        return ResponseEntity.ok(users);
     }
 }
 
--- backend/src/main/java/com/example/backend/user/UserService.java
+++ backend/src/main/java/com/example/backend/user/UserService.java
@@ -2,21 +2,22 @@ package com.example.backend.user;
 
 import com.example.backend.user.dtos.UserDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository repository){
+    public UserService(UserRepository repository) {
         userRepository = repository;
     }
 
-    public void registerUser(String nick, String password, int height, int weight, int birthyear){
+    public void registerUser(String nick, String password, int height, int weight, int birthyear) {
         var checkUser = userRepository.findByNick(nick);
-        if (checkUser != null){
+        if (checkUser != null) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "User with such username already exists in database");
         }
 
@@ -24,16 +25,21 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public UserDto loginUser(String nick, String password){
+    public UserDto loginUser(String nick, String password) {
         var checkUser = userRepository.findByNick(nick);
-        if(checkUser == null){
+        if (checkUser == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with nickname " + nick + " found");
         }
 
-        if(!checkUser.getPassword().equals(password)){
+        if (!checkUser.getPassword().equals(password)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provided password was incorrect");
         }
 
         return new UserDto(checkUser.getId(), checkUser.getNick());
+    }
+
+    public List<UserDto> getAll() {
+        var users = userRepository.findAll();
+        return users.stream().map(user -> new UserDto(user.getId(), user.getNick())).toList();
     }
 }
