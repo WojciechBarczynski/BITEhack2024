176ed02628342a1befefc1940dffbf9eebcb770f
Wojciech Barczyński <104033489+WojciechBarczynski@users.noreply.github.com>
2024-01-20 14:54:45 +0100

    entities added (#6)
    Co-authored-by: Arkadiusz Mincberger <410730@student.agh.edu.pl>

---
+++ backend/src/main/java/com/example/backend/user/User.java
@@ -0,0 +1,46 @@
+package com.example.backend.user;
+
+import com.example.backend.addiction.Addiction;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Table(indexes = {
+        @Index(columnList = "nick", unique = true)
+})
+public class User {
+    @Id
+    @GeneratedValue
+    @Getter private int id;
+    @Getter @Setter private String nick;
+    @Getter @Setter private int birthyear;
+    @Getter @Setter private int weightKg;
+    @Getter @Setter private int heightCm;
+
+    @ManyToMany
+    @Getter @Setter private List<Addiction> addictions;
+
+    public User(String nick, int birthyear, int weightKg, int heightCm){
+        this.nick = nick;
+        this.birthyear = birthyear;
+        this.heightCm = heightCm;
+        this.weightKg = weightKg;
+        this.addictions = new ArrayList<>();
+    }
+
+    public void addAddiction(Addiction addiction){
+        addictions.add(addiction);
+    }
+}
