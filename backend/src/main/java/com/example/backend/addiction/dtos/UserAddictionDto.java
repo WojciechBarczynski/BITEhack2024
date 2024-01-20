03f3a013649669c8d743d6088198fa773197bca0
Wojciech Barczynski <barczynski.w@gmail.com>
2024-01-20 22:16:28 +0100

    Predict messages return

--- backend/src/main/java/com/example/backend/addiction/dtos/UserAddictionDto.java
+++ backend/src/main/java/com/example/backend/addiction/dtos/UserAddictionDto.java
@@ -1,6 +1,4 @@
 package com.example.backend.addiction.dtos;
 
-import java.util.Optional;
-
-public record UserAddictionDto(int id, String name, Optional<Long> daysClean) {
+public record UserAddictionDto(int id, String name, Long daysClean) {
 }
--- backend/src/main/java/com/example/backend/report/dtos/RecordsForAddictionDto.java
+++ backend/src/main/java/com/example/backend/report/dtos/RecordsForAddictionDto.java
@@ -2,5 +2,5 @@ package com.example.backend.report.dtos;
 
 import java.util.List;
 
-public record RecordsForAddictionDto(String name, List<ReportDto> reports) {
+public record RecordsForAddictionDto(String name, String message, List<ReportDto> reports) {
 }
