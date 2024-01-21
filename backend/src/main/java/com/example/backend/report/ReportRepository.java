35324972ecf83016f3a6d81afea942eb8e13fb3e
szWozniak <szymek.woz@gmail.com>
2024-01-21 03:02:17 +0100

    quickfix

--- backend/src/main/java/com/example/backend/report/ReportRepository.java
+++ backend/src/main/java/com/example/backend/report/ReportRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 // Most of those methods are to prevent N+1
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
-    List<Report> findAllByRelation_Addict_IdAndRelation_Addiction_Id(int addictId, int addictionId);
+    List<Report> findAllByRelation_Addict_IdAndRelation_Addiction_IdOOrderByReportTimeDesc(int addictId, int addictionId);
 
     List<Report> findAllByRelation_Addict_Id(int addictId);
 
